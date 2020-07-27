eebb43a01befa9363e17ba7e5bade74570eacda8
vadash <vadash@gmail.com>
2020-07-27 12:55:40 +0300

    Initial commit

---
+++ Core/Core.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Forms;
+using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Cache;
+using ExileCore.Shared.Enums;
+using SharpDX;
+using Vector2 = System.Numerics.Vector2;
+
+namespace dankpact
+{
+    public class DrawSkills
+    {
+        
+    }
+    public class SkillDpsCore : BaseSettingsPlugin<Settings>
+    {
+        public override void Render()
+        {
+          
+          
+        }
+    }
+}
---
+++ Core/Settings.cs
@@ -0,0 +1,14 @@
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using SharpDX;
+
+namespace dankpact
+{
+    public class Settings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+
+
+    }
+}
